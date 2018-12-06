5f0a9fd9d823b9b3f31070b6d0898a5e412161d7
Riounis <samrappl50@gmail.com>
2018-12-05 20:42:50 -0500

    Added assigned users and pseudocode for gale shapeley

---
+++ app/assigned_users.py
@@ -0,0 +1,4 @@
+class AssignedUsers:
+    def __init__(self, teamid, users = []):
+        self.teamid = teamid
+        self.users = users
--- app/top_trading_cycles.py
+++ app/top_trading_cycles.py
@@ -1,6 +1,19 @@
 from user import User
+from random import shuffle
 
 def gale_shapley(users):
-	#run topic assignment here
-	return None
+    # map of team ids to collection of users reviewing them
+    assigned_users = []
     
+	# randomize users for fairness
+    shuffle(users)
+    
+    # choose the highest ranked team available for each user
+    for user in users:
+        
+        # if team id is in the map
+            # fewer than MAX REVIEWERS, assign user to this review
+            # MAX REVIEWERS, fall through to next topic
+        #if team id is not in the map, insert it and assign user to this review
+            
+	return assigned_users
