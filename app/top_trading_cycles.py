b67adcbec04b3f966bbb1c9e5a7883d0d704e26a
Riounis <samrappl50@gmail.com>
2018-12-05 20:54:47 -0500

    small progress

--- app/top_trading_cycles.py
+++ app/top_trading_cycles.py
@@ -10,10 +10,13 @@ def gale_shapley(users):
     
     # choose the highest ranked team available for each user
     for user in users:
+    
+        #for each team id in user.topic_rank
+        for team in user.topic_rank:
         
-        # if team id is in the map
-            # fewer than MAX REVIEWERS, assign user to this review
-            # MAX REVIEWERS, fall through to next topic
-        #if team id is not in the map, insert it and assign user to this review
+            # if team id is in the map
+                # fewer than MAX REVIEWERS, assign user to this review
+                # MAX REVIEWERS, fall through to next team id
+            #if team id is not in the map, insert it and assign user to this review
             
 	return assigned_users
